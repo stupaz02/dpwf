71b124fbb42b789deda1b24b511794b3ea6cb4d0
unknown <sergiotupaz@gmail.com>
2017-12-29 17:31:05 +0800

    added simplemde editor

--- resources/views/backend/post/create.blade.php
+++ resources/views/backend/post/create.blade.php
@@ -122,5 +122,22 @@
 @section('script')
   <script type="text/javascript">
         $('ul.pagination').addClass('no-margin pagination-sm');
+
+
+        $('#title').on('blur', function(){
+            var theTitle  = this.value.toLowerCase().trim(),
+                slugInput = $('#slug'),
+                theSlug   = theTitle.replace(/$/g, '-and-')
+                            .replace(/[^a-z0-9-]+/g,'-')
+                            .replace(/\-\-+/g, '-')
+                            .replace(/^-+|-+$/g, ' ');
+                            
+
+                slugInput.val(theSlug);
+        });
+
+        var simplemde1 = new SimpleMDE({ element: $("#excerpt")[0] });
+        var simplemde2 = new SimpleMDE({ element: $("#body")[0] });
   </script>
 @endsection
+
--- resources/views/layouts/backend/main.blade.php
+++ resources/views/layouts/backend/main.blade.php
@@ -20,7 +20,7 @@
   <link rel="stylesheet" href="/backend/css/skins/_all-skins.min.css">
   <!-- bootstrap wysihtml5 - text editor -->
   <!-- <link rel="stylesheet" href="/../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css"> -->
-
+  <link rel="stylesheet" href="/backend/plugins/simplemde/simplemde.min.css" >
   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
@@ -54,6 +54,7 @@
 <script src="/backend/js/jquery-2.2.3.min.js"></script>
 <!-- Bootstrap 3.3.6 -->
 <script src="/backend/js/bootstrap.min.js"></script>
+<script src="/backend/plugins/simplemde/simplemde.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/backend/js/app.min.js"></script>
 @yield('script')
